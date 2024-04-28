595076d781f17cd2c86417d229b62dbc01ea5684
David6790 <david.lb90@gmail.com>
2024-04-28 09:39:50 +0200

    ajout dbcontext et entities

---
+++ Data/ContextReservation.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mio_Rest_Api.Entities;
+
+namespace Mio_Rest_Api.Data
+{
+    public class ContextReservation : DbContext
+    {
+        public ContextReservation(DbContextOptions <ContextReservation> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Reservation> Reservations { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<OccupationStatus> OccupationStatus { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reservation>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e=>e.CreatedBy).HasMaxLength(50).IsUnicode(true);
+                entity.Property(e=>e.UpdatedBy).HasMaxLength(50).IsUnicode(true);
+                entity.Property(e=>e.OccupationStatusOnBook).HasMaxLength(20).IsUnicode(true);
+                entity.Property(e=>e.Comment).HasMaxLength(500).IsUnicode(true);
+                entity.Property(e=>e.Placed).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e=>e.IsPowerUser).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e=>e.Status).HasMaxLength(1).IsUnicode(false);
+
+
+
+            });
+
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e=>e.Name).HasMaxLength(50).IsUnicode(true);
+                entity.Property(e=>e.Prenom).HasMaxLength(50).IsUnicode(true);
+                entity.Property(e=>e.Telephone).HasMaxLength(50).IsUnicode(true);
+                entity.Property(e=>e.Email).HasMaxLength(255).IsUnicode(true);
+                entity.Property(e=>e.FreeTable21).HasMaxLength(1).IsUnicode(false);
+
+
+            });
+
+            modelBuilder.Entity<OccupationStatus>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e=>e.OccStatus).HasMaxLength(20).IsUnicode(true);
+
+            });
+        }
+    }
+
+
+}
---
+++ Entities/Reservation.cs
@@ -0,0 +1,39 @@
+﻿namespace Mio_Rest_Api.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public Guid IdClient { get; set; }
+        public DateOnly DateResa { get; set; }
+        public TimeOnly TimeResa { get; set; }
+        public int NumberOfGuest { get; set; }
+        public DateTime CreaTimeStamp { get; set; }
+        public DateTime? UpdateTimeStamp { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public string? UpdatedBy { get; set; }
+        public string? Comment { get; set; }
+        public string Placed { get; set; } = "N";
+        public string IsPowerUser { get; set; } = "N";
+        public string Status { get; set; } = "P";
+        public string OccupationStatusOnBook { get; set; } = "RAS";
+    }
+
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Prenom { get; set; } = string.Empty;
+        public string? Telephone { get; set; } 
+        public string? Email { get; set; }
+        public string FreeTable21 { get; set; } = string.Empty;
+    }
+
+    public class OccupationStatus
+    {
+        public int Id { get; set; }
+        public DateTime DateOfEffect { get; set; }
+        public string OccStatus { get; set; } = "RAS";
+
+    }
+}
